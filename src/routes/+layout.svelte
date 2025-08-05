b735f0ec749b8126da1388ab5167fa828f360638
Micha Mailaender <micha@mailaender.me>
2025-08-05 10:58:03 +0200

    refactor: validate isAuthenticated via setAuth callback instead of API query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,8 @@
 	import '../app.css';
 	import { createSvelteAuthClient } from '$lib/svelte/index.js';
 	import { authClient } from '$lib/auth-client.js';
-	import { api } from '$convex/_generated/api.js';
 
-	createSvelteAuthClient({ authClient, api })
+	createSvelteAuthClient({ authClient });
 
 	let { children } = $props();
 </script>
